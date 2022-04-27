c25ba05c0601624ee49e9b3ba2a92cdd81bd420d
prudoss25 <prudoss26@gmail.com>
2022-04-27 19:45:08 +0000

    init rooting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React,{ Component } from 'react';
-import Home from './containers/Home/Home';
-import Layout from './hoc/Layout/Layout';
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./containers/About/About";
+import Contacts from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
+import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
-       <Layout>
-          <Home />
-       </Layout>
+        <Layout>
+          <Routes>
+            <Route path="/" exact element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contacts" element={<Contacts/>} />
+          </Routes>
+        </Layout>
       </div>
     );
   }
