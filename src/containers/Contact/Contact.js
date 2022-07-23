6e9901f1c3c18ea75d1e0fe3d932d85baa94bcae
prudoss25 <prudoss26@gmail.com>
2022-07-23 17:52:31 +0100

    update contacts

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,16 +1,44 @@
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardHeader,
+} from "@material-ui/core";
 import React, { Component } from "react";
 import Aux from "../../hoc/_Aux/_Aux";
+import Formulaire from "./Formulaire/Formulaire";
 
+import AdresseMail from "./ContactezNous/AdresseMail/AdresseMail";
+import ReseauxSociaux from "./ContactezNous/ReseauxSociaux/ReseauxSociaux";
+import Telephone from "./ContactezNous/Telephone/Telephone";
 
 class Contacts extends Component {
-   render(){
-    return(
-        <Aux>
-            Contacts
-        </Aux>
+  render() {
+    return (
+      <Aux>
+        <Grid>
+          <Card>
+            <CardHeader
+              title="Contactez-Nous"
+              subheader="Ci-joint les différents canaux par lesquels nous sommes
+        joignables. N'hésitez pas à nous joindre ou nous envoyer un
+        message via le formulaire."
+            />
+            <CardContent>
+              <Grid container justifyContent="space-around">
+                <AdresseMail />
+                <Telephone />
+                <ReseauxSociaux />
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid style={{ padding: "20px 5px" }}>
+          <Formulaire />
+        </Grid>
+      </Aux>
     );
-   }
-    
+  }
 }
 
 export default Contacts;
--- src/containers/Contact/ContactezNous/ReseauxSociaux/ReseauxSociaux.js
+++ src/containers/Contact/ContactezNous/ReseauxSociaux/ReseauxSociaux.js
@@ -1,23 +1,46 @@
 import React from "react";
-import Aux from "../../../../hoc/_Aux/_Aux";
-import facebook from "../../../../assets/images/facebook.png";
-import instagram from "../../../../assets/images/instagram.png";
-import linkedin from "../../../../assets/images/linkedin.png";
-import classes from './ReseauxSociaux.css';
+import { Grid, List, ListItem, ListItemAvatar, ListItemText, Avatar } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import RubriqueTitle from "../../../../components/UI/RubriqueTitle/RubriqueTitle";
 
 
-const ReseauxSociaux = () => {
-    return (
-        <Aux>
-            <p className={classes.ReseauxSociaux}>
-                <h1>Nos Reseaux Sociaux</h1>
-                <ul>
-                    <li><a href="/"><img src={facebook} alt="Facebook"/></a></li>
-                    <li><a href="/"><img src={instagram} alt="Instagram"/></a></li>
-                    <li><a href="/"><img src={linkedin} alt="Linkedin"/></a></li>
-                </ul>
-            </p>
-        </Aux>
-    )
+const reseauxList = [
+  {
+    name:"ASEBEM Nationale",
+    link:"https://web.facebook.com/AsebemOfficiel",
+    icon:<FacebookIcon/>
+  },
+  {
+    name:"ASEBEM Nationale",
+    link:"https://www.instagram.com/asebem_nationale/",
+    icon:<InstagramIcon />
+  }
+]
+export default function ReseauxSociaux() {
+  
+
+  return (
+      <Grid  item >
+          <RubriqueTitle>
+            Reseaux Sociaux
+          </RubriqueTitle>
+          <List dense>
+              {reseauxList.map((reseau, index) => 
+                <ListItem
+                key={index}
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      {reseau.icon}
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={reseau.name}
+                  />
+                </ListItem>
+              )}
+            </List>
+      </Grid>
+  );
 }
-export default ReseauxSociaux
