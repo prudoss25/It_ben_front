38d4bd089f103908cf0502a26f5bdb906a24a193
prudoss25 <prudoss26@gmail.com>
2022-08-13 19:25:21 +0100

    fix bugs

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,7 @@ import Statistiques from "./Statistiques/Statistiques";
 import Partenaires from "./Partenaires/Partenaires";
 import HeroSection from "./HeroSection/HeroSection";
 import Evenement from "./Evenements/Evenement";
-import classes from "./Home.css";
+// import classes from "./Home.css";
 import GaleriePhoto from "./GaleriePhoto/GaleriePhoto";
 import Actualite from "./Actualites/Actualitee";
 import Footer from "../../components/Footer/Footer";
