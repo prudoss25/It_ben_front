45c3567903443fea83a402cecca9ba0909a36b87
prudoss25 <prudoss26@gmail.com>
2022-09-06 22:24:33 +0100

    corrections sur lajout de liste de membres

--- src/containers/Users/AddButton/AddButton.js
+++ src/containers/Users/AddButton/AddButton.js
@@ -2,118 +2,108 @@ import React, { useState } from "react";
 import * as XLSX from "xlsx";
 import Aux from "../../../hoc/_Aux/_Aux";
 import UserTable from "./UserTable";
-//import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import {Grid } from '@material-ui/core';
+import Button from "@mui/material/Button";
+import FormModal from "../../../common/FormModal/FormModal";
+import { Grid } from "@material-ui/core";
 import DownloadButton from "./DownloadButton";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
-
-
-
-
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function AddButton() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const [rows, setRows] =useState(null);
-   const [fileName, setFileName] = useState(null);
-  const handleOnChange= async (e) => {
-    let dataFinal=[];
-    const file =e.target.files[0];
+  
+  const [rows, setRows] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const handleClose = () => {
+    setRows(null)
+    setFileName(null)
+    setOpen(false)
+  };
+  const handleOnChange = async (e) => {
+    let dataFinal = [];
+    const file = e.target.files[0];
 
     setFileName(file.name);
-    const readOpts = { 
-      cellText:false, 
-      cellDates:true
+    const readOpts = {
+      cellText: false,
+      cellDates: true,
     };
     const jsonOpts = {
       header: 0,
-  defval: '',
-  blankrows: true,
-  raw: false,
-  dateNF: 'd"/"mm"/"yyyy' 
-}
+      defval: "",
+      blankrows: true,
+      raw: false,
+      dateNF: 'd"/"mm"/"yyyy',
+    };
     const data = await file.arrayBuffer();
-    const workbook = XLSX.read(data,readOpts);
+    const workbook = XLSX.read(data, readOpts);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-     const jsonData = XLSX.utils.sheet_to_json(worksheet,jsonOpts);
-     console.log("TEST DATATABLE jsonData",jsonData)
-       dataFinal = jsonData.filter(row => row.Nom !=="" && row.Nom!=null
-       && row.Prenom !=="" && row.Prenom!=null
-       && row.AddresseMail !=="" && row.AddresseMail!=null
-       && row.DateDeNaissance!=="" && row.DateDeNaissance!=null
-       && row.Telephone !=="" &&row.Telephone!=null
-       && row.Ville !=="" && row.Ville!=null
-       )
-       setRows(dataFinal);
-       
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, jsonOpts);
+    dataFinal = jsonData.filter(
+      (row) =>
+        row.Nom !== "" &&
+        row.Nom != null &&
+        row.Prenom !== "" &&
+        row.Prenom != null &&
+        row.AddresseMail !== "" &&
+        row.AddresseMail != null &&
+        row.DateDeNaissance !== "" &&
+        row.DateDeNaissance != null &&
+        row.Telephone !== "" &&
+        row.Telephone != null &&
+        row.Ville !== "" &&
+        row.Ville != null
+    );
+    setRows(dataFinal);
   };
-
-    
-
 
   return (
     <Aux>
-      <Button variant = "contained" onClick={handleOpen}>Ajouter via excel</Button>
-      <Modal
+      <Button variant="contained" onClick={handleOpen}>
+        Ajouter via excel
+      </Button>
+      <FormModal
         open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        title="Enregistrer une liste de Membres"
+        handleClose={handleClose}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Téléchargez la template puis remplissez la
-          </Typography>
-
-          <Grid>
-              <DownloadButton/>
+        <form>
+          <Grid container>
+            <Grid item xs={12}>
+              <DownloadButton />
+            </Grid>
+            <Grid item xs={12}>
+              {fileName && (
+                <p>
+                  Fichier : <span>{fileName}</span>
+                </p>
+              )}
+              <input
+                onChange={(e) => handleOnChange(e)}
+                style={{ display: "none" }}
+                accept=".xlsx"
+                id="contained-button-file"
+                multiple={false}
+                type="file"
+              />
+              <label htmlFor="contained-button-file">
+                <Button
+                  variant="contained"
+                  fullWidth
+                  startIcon={<CloudUploadIcon />}
+                  component="span"
+                >
+                  Chargez la liste Remplie
+                </Button>
+              </label>
+              <Grid>
+                {fileName && rows && <UserTable handleClose={handleClose} dataRows={rows}></UserTable>}
+              </Grid>
+            </Grid>
           </Grid>
-
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Uploadez le fichier rempli 
-          </Typography>
-          
-          <Grid>
-          {fileName && (
-            <p>
-              Fichier: : <span>{fileName}</span>
-            </p>
-          )}
-          <input 
-            type="file"
-            onChange={(e) => handleOnChange(e) }
-          />
-          <Grid>
-            {fileName && rows &&(
-              <UserTable dataRows ={rows}></UserTable>
-            )}
-          </Grid>
-          
-          
-          
-        </Grid>
-
-
-        </Box>
-      </Modal>
+        </form>
+      </FormModal>
     </Aux>
   );
 }
-
--- src/containers/Users/AddButton/UserTable.js
+++ src/containers/Users/AddButton/UserTable.js
@@ -1,20 +1,20 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState } from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Grid } from '@material-ui/core';
-import Button from '@mui/material/Button';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Grid } from "@material-ui/core";
+import Button from "@mui/material/Button";
 import axios from "../../../axios";
-import {  ADD_ALL_USERS } from "../../../Routes";
+import { ADD_ALL_USERS } from "../../../Routes";
 import "moment/locale/fr";
-import { ROLES } from '../../../Constantes';
-import NoticationAlert from '../../../components/UI/NotificationAlert/NotificationAlert';
-import Aux from '../../../hoc/_Aux/_Aux';
+import { ROLES } from "../../../Constantes";
+import NoticationAlert from "../../../components/UI/NotificationAlert/NotificationAlert";
+import Aux from "../../../hoc/_Aux/_Aux";
 
 export default function UserTable(props) {
   const [notification, setNotification] = useState({
@@ -40,45 +40,38 @@ export default function UserTable(props) {
     });
   };
 
-
-    const rows = props.dataRows;
-    const onSave = () => {
-
-      
-      const AllUserInfos =rows.map(row => 
-        {
-          const birthDate = row.DateDeNaissance.split('/');
-          return({
+  const rows = props.dataRows;
+  const onSave = () => {
+    const AllUserInfos = rows.map((row) => {
+      const birthDate = row.DateDeNaissance.split("/");
+      return {
         registrationDate: new Date(),
         startFunctionDate: new Date(),
         profilePicture: "Not Set",
-        lastName:row.Nom,
-        firstName:row.Prenom,
-        userName:`${row.Nom}_${row.Prenom}`,
-        password:Math.random().toString(36).substring(8),
-        emailAddress:row.AddresseMail,
-        birthDate:new Date(`${birthDate[2]}-${birthDate[1]}-${birthDate[0]}`),
-        phoneNumber:row.Telephone,
-        city:row.Ville,
-        role:ROLES.Membre
-      })});
+        lastName: row.Nom,
+        firstName: row.Prenom,
+        userName: `${row.Nom}_${row.Prenom}`,
+        password: Math.random().toString(36).substring(8),
+        emailAddress: row.AddresseMail,
+        birthDate: new Date(`${birthDate[2]}-${birthDate[1]}-${birthDate[0]}`),
+        phoneNumber: row.Telephone,
+        city: row.Ville,
+        role: ROLES.Membre,
+      };
+    });
 
-        axios
-          .post(ADD_ALL_USERS, AllUserInfos)
-          .then((response) => 
-          {
-            if (response.status === 200)
-              openNotification(
-                "success",
-                `Les Utilisateurs ont été ajouté!`
-              );
-          })
-          .catch(() => {
-            openNotification("error", "Une erreur est survenue !");
-          }
-          );
-      
-    };
+    axios
+      .post(ADD_ALL_USERS, AllUserInfos)
+      .then((response) => {
+        if (response.status === 200){
+          openNotification("success", `Les Utilisateurs ont été ajouté!`);
+          props.handleClose()
+        }
+      })
+      .catch(() => {
+        openNotification("error", "Une erreur est survenue !");
+      });
+  };
 
   return (
     <Aux>
@@ -86,7 +79,7 @@ export default function UserTable(props) {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="right" >Nom</TableCell>
+              <TableCell align="right">Nom</TableCell>
               <TableCell align="right">Prenom</TableCell>
               <TableCell align="right">AddresseMail</TableCell>
               <TableCell align="right">Date de Naissance</TableCell>
@@ -98,9 +91,8 @@ export default function UserTable(props) {
             {rows.map((row) => (
               <TableRow
                 key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-              
                 <TableCell align="right">{row.Nom}</TableCell>
                 <TableCell align="right">{row.Prenom}</TableCell>
                 <TableCell align="right">{row.AddresseMail}</TableCell>
@@ -113,11 +105,9 @@ export default function UserTable(props) {
         </Table>
       </TableContainer>
       <Grid>
-              <Button onClick={onSave}>Ajouter</Button>
+        <Button onClick={onSave}>Ajouter</Button>
       </Grid>
       <NoticationAlert handleClose={onNotificationClosed} {...notification} />
     </Aux>
   );
- 
-
 }
