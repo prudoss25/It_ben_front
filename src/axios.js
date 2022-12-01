e5d1c83355326360c8ce316a8d7c1c06b23bfff6
Prudence Dossou <prudoss26@gmail.com>
2022-12-01 14:35:15 +0100

    fix variabilisation problem

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.IT_BEN_API_BASE_URL
+    baseURL: process.env.REACT_APP_IT_BEN_API_BASE_URL
 });
 
 export const morrocoTownFetcher = axios.create({
-    baseURL:process.env.MOROCCO_TOWNS
+    baseURL:process.env.REACT_APP_MOROCCO_TOWNS
 })
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
